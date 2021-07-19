20c62465a686ab7e1c1dddbf0e47f0052ca64c61
vikkimccrory <victoriajmccrory@gmail.com>
2021-07-19 15:44:49 -0400

    Remove mangos

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,10 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from .models.mango import Mango
 from .models.user import User
 from .models.post import Post
 
-class MangoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mango
-        fields = ('id', 'name', 'color', 'ripe', 'owner')
+
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
